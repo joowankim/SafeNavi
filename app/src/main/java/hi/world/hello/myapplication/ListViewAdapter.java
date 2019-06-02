1cac8187e9e4283dc37c4421cef95430c2d80739
kjw217 <rlawndhks217@naver.com>
2019-06-02 13:56:48 +0900

    Update ListViewAdapter.java

--- app/src/main/java/hi/world/hello/myapplication/ListViewAdapter.java
+++ app/src/main/java/hi/world/hello/myapplication/ListViewAdapter.java
@@ -13,34 +13,48 @@ import com.skt.Tmap.TMapPOIItem;
 
 import java.util.List;
 
+/// @class ListViewAdapter
+/// @brief show ListView
 public class ListViewAdapter extends BaseAdapter {
 
     private Context context;
     private List<TMapPOIItem> list;
     private LayoutInflater inflate;
     private ViewHolder viewHolder;
-
+    
+    /// @brief constructor of ListViewAdapter
+    /// @param list list
+    /// @param context context of current scene
     public ListViewAdapter(List<TMapPOIItem> list, Context context) {
         this.list = list;
         this.context = context;
         this.inflate = LayoutInflater.from(context);
     }
-
+    
+    /// @brief get list size
+    /// @return list size
     @Override
     public int getCount(){
         return list.size();
     }
-
+    /// @brief get item id
+    /// @param position current item's location in ListView
+    /// @return current position of the item
     @Override
     public long getItemId(int position){
         return position;
     }
-
+    /// @brief get item
+    /// @param position current item's location in ListView
+    /// @return the pointing item information
     @Override
     public Object getItem(int position){
         return list.get(position);
     }
-
+    /// @brief floating view of item on list
+    /// @param position item's position
+    /// @param covertView view
+    /// @param parent 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         if (convertView == null){
@@ -54,7 +68,6 @@ public class ListViewAdapter extends BaseAdapter {
             viewHolder = (ViewHolder)convertView.getTag();
         }
         try {
-            // 아이템 내 각 위젯에 데이터 반영
             viewHolder.label.setText(list.get(position).getPOIName().toString());
         } catch(Exception e){
             e.printStackTrace();
