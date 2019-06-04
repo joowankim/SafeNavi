1bb3d106c9d49d2baabdd6fc4a1ec5ea8d973b3f
didi0410 <taristmas@gmail.com>
2019-06-04 17:14:31 +0900

    add arduino folder

---
+++ arduino/safenavi.ino
@@ -0,0 +1,75 @@
+#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "ssd1306.h"
+
+
+#define LED_PIN 13
+int bluetoothTx = 7;
+int bluetoothRx = 8;
+#define WIDTH     128
+#define HEIGHT     64
+SSD1306_begin();
+SSD1306_clear(oled_buf);
+
+SoftwareSerial mySerial(bluetoothTx, bluetoothRx); // RX, TX  
+// Connect HM10      Arduino Uno
+//     Pin 1/TXD          Pin 7
+//     Pin 2/RXD          Pin 8
+
+uint8_t oled_buf[WIDTH * HEIGHT / 8];
+
+void setup() {  
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  SSD1306_begin();
+  SSD1306_clear(oled_buf);
+
+}
+
+
+void loop() {  
+  String op;
+  
+  if (mySerial.available()) {
+    op = mySerial.read();  
+    
+    if (op != 0)
+    {
+      // about signs
+      if(op.startsWith("0"){
+        String t = op.substring(2);
+        if(t[0].c_str() == '0'){
+          // straight sign
+          SSD1306_bitmap(30, 40, up_map, 16, 8, oled_buf); 
+        } else if(t[0].c_str() == '1'){
+          // right straight sign
+          SSD1306_bitmap(30, 40, right_map, 16, 8, oled_buf); 
+        } else if(t[0].c_str() == '2'){
+          // left straight sign
+          SSD1306_bitmap(35, 30, left_map, 16, 8, oled_buf); 
+        } else if(t[0].c_str() == '3'){
+          // turn left sign
+          SSD1306_bitmap(25, 40, go_left_map, 16, 8, oled_buf); 
+        } else if(t[0].c_str() == '4'){
+          // turn right sign
+          SSD1306_bitmap(25,30, go_right_map, 16, 8, oled_buf); 
+        } else if(t[0].c_str() == '5'){
+          // return sign
+          SSD1306_bitmap(20, 40, go_back_map, 16, 8, oled_buf); 
+        } else if(t[0].c_str() = ='6'){
+          // go backward sign
+          SSD1306_bitmap(0, 2, down_map, 16, 8, oled_buf); 
+        }
+      }
+
+      // about other informations.
+      else if(op.startsWith("1")){
+        String dist = op.substring(2);
+        dist.concat(" m");
+        SSD1306_string(98, 52, dist, 12, 0, oled_buf);
+      }
+    }
+    
+  }
+}
